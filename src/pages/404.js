579a36ea6fde17b5c753a9814313637206dcb242
Max Hammond <max.hammond@pushon.co.uk>
2020-06-01 14:40:16 +0100

    remove page 2 and edit 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
+import {Link} from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404</h1>
+    <p>Seems this link doesn't go anywhere. Whoops!</p>
+    <Link to="/">Back</Link>
   </Layout>
 )
 
